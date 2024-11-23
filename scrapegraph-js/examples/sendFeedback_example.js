a23e86c74cad9e5ab788ab233c03728801d52215
DPende <pendesini.daniele@gmail.com>
2024-11-23 15:27:46 +0100

    add: added functions examples

---
+++ scrapegraph-js/examples/sendFeedback_example.js
@@ -0,0 +1,14 @@
+import { sendFeedback } from "scrapegraph-sdk";
+import 'dotenv/config';
+
+try {
+  const apiKey = process.env.SGAI_APIKEY;
+  const requestId = "16a63a80-c87f-4cde-b005-e6c3ecda278b";
+  const rating = 5;
+  const feedbackMessage = "This is a test feedback message.";
+
+  const feedback_response = await sendFeedback(apiKey, requestId, rating, feedbackMessage);
+  console.log(feedback_response);
+} catch (error) {
+  console.error(error)
+}
---
+++ scrapegraph-js/examples/smartScraper_example.js
@@ -0,0 +1,14 @@
+import { smartScraper } from "scrapegraph-sdk";
+import 'dotenv/config';
+
+try {
+  const apiKey = process.env.SGAI_APIKEY;
+  const url = "https://scrapegraphai.com";
+  const prompt = "What does the company do?";
+
+  const response = await smartScraper(apiKey, url, prompt);
+
+  console.log(response);
+} catch (error) {
+  console.error(error);
+}
