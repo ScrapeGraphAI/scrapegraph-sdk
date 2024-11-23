40fafb36c7c5274d27627413c22ae3fe1d25bfdc
DPende <pendesini.daniele@gmail.com>
2024-11-23 15:27:46 +0100

    add: added functions examples

---
+++ scrapegraph-js/examples/getCredits_example.js
@@ -0,0 +1,12 @@
+import { getCredits } from "scrapegraph-sdk";
+import 'dotenv/config';
+
+try {
+    const apiKey = process.env.SGAI_APIKEY;
+
+    const myCredit = await getCredits(apiKey);
+
+    console.log(myCredit)
+} catch (error) {
+    console.error(error)
+}
