6270a6e43782f9efbb407e71b1ca7c57c37db38a
DPende <pendesini.daniele@gmail.com>
2024-11-25 22:06:47 +0100

    refactor: code refactoring

--- scrapegraph-js/examples/smartScraper_example.js
+++ scrapegraph-js/examples/smartScraper_example.js
@@ -1,10 +1,10 @@
-import { smartScraper } from "scrapegraph-sdk";
+import { smartScraper } from 'scrapegraph-sdk';
 import 'dotenv/config';
 
 try {
   const apiKey = process.env.SGAI_APIKEY;
-  const url = "https://scrapegraphai.com";
-  const prompt = "What does the company do?";
+  const url = 'https://scrapegraphai.com';
+  const prompt = 'What does the company do?';
 
   const response = await smartScraper(apiKey, url, prompt);
 
