a2b57c7e482dfb5c7c1a125d1684e0367088c83b
DPende <pendesini.daniele@gmail.com>
2024-11-29 22:13:21 +0100

    refactor: code refactoring

--- scrapegraph-js/examples/getSmartScraperRequest_example.js
+++ scrapegraph-js/examples/getSmartScraperRequest_example.js
@@ -1,12 +1,11 @@
-import { getSmartScraperRequest } from 'scrapegraph-sdk';
+import { getSmartScraperRequest } from 'scrapegraph-js';
 import 'dotenv/config';
 
+const apiKey = process.env.SGAI_APIKEY;
+const requestId = '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+
 try {
-  const apiKey = process.env.SGAI_APIKEY;
-  const requestId = '3fa85f64-5717-4562-b3fc-2c963f66afa6'
-
   const requestInfo = await getSmartScraperRequest(apiKey, requestId);
-
   console.log(requestInfo);
 } catch (error) {
   console.error(error);
