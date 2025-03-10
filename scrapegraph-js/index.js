a342525091f7f0a7e72f7fd4b60c2f7bf71f619e
Marco Vinciguerra <mvincig11@gmail.com>
2025-03-10 11:24:58 +0100

    removed local scraper from js

--- scrapegraph-js/index.js
+++ scrapegraph-js/index.js
@@ -1,6 +1,5 @@
 export { smartScraper, getSmartScraperRequest } from './src/smartScraper.js';
 export { markdownify, getMarkdownifyRequest } from './src/markdownify.js';
-export { localScraper, getLocalScraperRequest } from './src/localScraper.js';
 export { searchScraper, getSearchScraperRequest } from './src/searchScraper.js';
 export { getCredits } from './src/credits.js';
 export { sendFeedback } from './src/feedback.js';
